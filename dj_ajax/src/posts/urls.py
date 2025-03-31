32c59be2480861d13df8e45ff75120ff0f9d8e9f
Jose Morales <jmoralesgutierr4264@conestogac.on.ca>
2025-03-31 18:49:05 -0400

    Create the view for image uploading

--- dj_ajax/src/posts/urls.py
+++ dj_ajax/src/posts/urls.py
@@ -9,6 +9,7 @@ from .views import (
     post_detail_data_view,
     delete_post,
     update_post,
+    image_upload_view
 
 )
 
@@ -18,6 +19,7 @@ urlpatterns = [
     re_path(r'^favicon\.ico/?$', lambda request: HttpResponse(status=204)),#
     path('', post_list_and_create, name='main-board'),
     path('like-unlike/', like_unlike_post, name='like-unlike'),
+    path('upload/', image_upload_view, name='image-upload'),
     path('<pk>/', post_detail, name='post-detail'),
     path('<pk>/update/', update_post, name='post-update'),
     path('<pk>/delete/', delete_post, name='post-delete'),
